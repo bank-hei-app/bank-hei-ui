97fa6ed86cbc16f52209e2c6548a59d619452d61
Fifaliana <hei.fifaliana@gmail.com>
2024-03-21 16:00:24 +0300

    feat: use axios for fetching data

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import { Account } from "./account";
+
+export function MainContent(props) {
+  const { editingUser, setEditingUser, setEditModal } = props;
+  const [bankAccounts, setBankAccounts] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://127.0.0.1:8080/accounts")
+      .then((response) => {
+        setBankAccounts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error when fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <section id="main-content">
+      {bankAccounts.map((account, index) => (
+        <Account
+          key={index}
+          index={index}
+          fullname={account.clientLastName}
+          firstname={account.clientName}
+          bank={account.bankName}
+          accountNumber={account.accountNumber}
+          balance={account.defaultSolde}
+          editingUser={editingUser}
+          setEditingUser={setEditingUser}
+          setEditModal={setEditModal}
+        />
+      ))}
+    </section>
+  );
+}
