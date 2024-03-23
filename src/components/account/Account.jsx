5dbf0ba0cf740d04e2850be65b55d65ca77eef8d
Fifaliana <hei.fifaliana@gmail.com>
2024-03-23 14:15:45 +0300

    fix: remove index props

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,61 +1,54 @@
-import { ActionButtons } from "../ActionButtons";
 import { formatNumber } from "../../utils/Utils";
-
+import EditIcon from "@mui/icons-material/Edit";
 export function Account(props) {
-  const { bank,accountNumber, balance, fullname, firstname,editingUser, setEditingUser, index,  setEditModal} = props;
-    
-  const action = <ActionButtons
-    index={index}
-    editingUser={editingUser}
-    setEditingUser={setEditingUser}
-    setEditModal={setEditModal}
-    
-  />;
-  
+  const {
+    bank,
+    accountNumber,
+    balance,
+    fullname,
+    firstname,
+    onEditClick
+  } = props;
+
   return (
     <div className="account">
-        <div className="details">
-            <AccountHolder fullname={fullname}  />
-            <AccountName firstname={firstname} />
-            <AccountNumber accountNumber={accountNumber} />
-            {action}
+      <div className="details">
+        <AccountHolder fullname={fullname} />
+        <AccountName firstname={firstname} />
+        <AccountNumber accountNumber={accountNumber} />
+        <div id="actions">
+          <button onClick={onEditClick}>
+            <EditIcon />
+            Edit
+          </button>
         </div>
-            <BankName bank={bank} />
-        <AccountBalance balance={formatNumber(balance)} />
+      </div>
+      <BankName bank={bank} />
+      <AccountBalance balance={formatNumber(balance)} />
     </div>
-  )
+  );
 }
 
 export const AccountHolder = (props) => {
   return (
-    <h1>{props.fullname} {props.firstname}</h1>
-  )
-}
+    <h1>
+      {props.fullname} {props.firstname}
+    </h1>
+  );
+};
 export const AccountName = (props) => {
-  return (
-    <h1>{props.firstname}</h1>
-  )
-}
-
+  return <h1>{props.firstname}</h1>;
+};
 
 export const BankName = (props) => {
-  return (
-    <div>{props.bank}</div>
-  )
-}
-
+  return <div>{props.bank}</div>;
+};
 
 export const AccountNumber = (props) => {
-  return (
-    <div>{props.accountNumber}</div>
-  )
-}
-
+  return <div>{props.accountNumber}</div>;
+};
 
 export const AccountBalance = (props) => {
   const balance = props.balance;
-  return (
-    <div className="balance">{balance}</div>
-  )
-}
-
+  return <div className="balance">{balance}</div>;
+};
